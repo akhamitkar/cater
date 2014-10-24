696eb59fd7b8c35006f0a7d6aa11a5f86a0fa098
saasbook <saasbook@saasbook.(none)>
2014-10-24 19:41:36 -0400

    Removing crud

--- db/migrate/20140819161406_create_caterers.rb
+++
@@ -1,9 +0,0 @@
-class CreateCaterers < ActiveRecord::Migration
-  def change
-    create_table :caterers do |t|
-      t.string :caterername
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140929130045_create_depts.rb
+++
@@ -1,9 +0,0 @@
-class CreateDepts < ActiveRecord::Migration
-  def change
-    create_table :depts do |t|
-      t.string :dept_name
-
-      t.timestamps
-    end
-  end
-end
